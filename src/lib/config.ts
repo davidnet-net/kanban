b7ab3124d53fbec66d91bf7c4a169105f56b834c
dedestem <david@davidnet.net>
2025-09-09 17:05:59 +0200

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-//export const authapiurl = "https://auth.davidnet.net/";
-//export const authapiurl = "https://kanban-api.davidnet.net/";
-export const authapiurl = "http://localhost:1000/";
-export const kanbanapiurl = "http://localhost:1001/";
+export const authapiurl = "https://auth.davidnet.net/";
+export const kanbanapiurl = "https://kanban-api.davidnet.net/";
+//export const authapiurl = "http://localhost:1000/";
+//export const kanbanapiurl = "http://localhost:1001/";
